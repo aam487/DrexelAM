760cff60761eb92d85281b39d4fa25ba89ea6b93
jrobins21 <jamie.daley98@gmail.com>
2025-03-12 15:39:33 -0400

    Adding comments

--- public/js/login.js
+++ public/js/login.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const loginButton = document.querySelector(".signup-box .button");
   const cancelButton = document.querySelector(".signup-box .button1");
 
+// Event listener for login button
   loginButton.addEventListener("click", async (e) => {
     e.preventDefault();
+    // get username and password
     const username = usernameInput.value.trim();
     const password = passwordInput.value;
-
+	// try to login with credentials
 	try {
         const response = await fetch('/auth/login', {
             method: 'POST',
@@ -18,14 +20,14 @@ document.addEventListener("DOMContentLoaded", () => {
             credentials: 'same-origin',  // Ensures cookies (for sessions) are sent
             body: JSON.stringify({ username, password })
         });
-
+        // report error
         if (!response.ok) {
             const errorText = await response.text();
             throw new Error(errorText || "Login failed.");
         }
 
         const data = await response.json();
-
+	// if the user is an author send them to author dashboard, if reader go to reader dashboard
         if (data.success) {
             window.location.href = data.role === 'author' ? '/author_dash.html' : '/reader_dash.html';
         } else {
@@ -37,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("An error occurred during login. Please try again.");
     }
 });
-
+  // event listener for cancel button, return to welcome page
   cancelButton.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "welcomepage.html";
